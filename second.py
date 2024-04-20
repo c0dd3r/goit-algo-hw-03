29a5bfb781c40210324f042f429342d50b7b21a6
c0dd3r <hmwrkrst@gmail.com>
2024-04-20 21:12:35 +0300

    second try

--- second.py
+++ second.py
@@ -1,8 +1,18 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    if min < 1 or max > 1000 or quantity < min or quantity > max:
-        return []
-    numbers = random.sample(range(min, max + 1), quantity)
-    numbers.sort()
-    return numbers
+    if min < 1 or max > 1000 or quantity < min or quantity > max or quantity > (max - min + 1):
+        return []  
+    
+    unique_numbers = set()
+    
+    while len(unique_numbers) < quantity:
+        number = random.randint(min, max)
+        unique_numbers.add(number)
+    sorted_numbers = sorted(unique_numbers)
+    
+    return sorted_numbers
+
+
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
